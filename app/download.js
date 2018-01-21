47fbdf3487e611a04b26405779c728db61e23fc5
snutiise <snutiise@hanmail.net>
2018-01-21 14:07:05 +0900

    remove update

--- app/download.js
+++ app/download.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
                     else if(String(json[i]['type']).indexOf("audio")>-1) icon='glyphicon glyphicon-music';
                     else if(String(json[i]['type']).indexOf("zip")>-1 || String(json[i]['type']).indexOf("7z")>-1 || String(json[i]['type']).indexOf("rar")>-1 || String(json[i]['type']).indexOf("bz")>-1 || String(json[i]['type']).indexOf("tar")>-1) icon='glyphicon glyphicon-compressed';
 
-                    let list="<a href='http://sodeok.xyz:8002/download/"+String(json[i]["code"])+"'><div class='file'><span class='size'>"+byteConvertor(json[i]['size'])+"</span><span class='date'>"+new Date(Number(json[i]['date'])).toString()+"</span><span class='img "+icon+"'></span><span>"+ String(json[i]["filename"]) + "</span></div></a>";
+                    let list="<a href='http://sodeok.xyz:8002/download/"+String(json[i]["code"])+"'><div class='file'><span class='size'>"+byteConvertor(json[i]['size'])+"</span><span class='date'>"+new Date(Number(json[i]['date'])).toString()+"</span><span class='img "+icon+"'></span><span>"+ String(json[i]["filename"]) + "</span><span class='remove glyphicon glyphicon-remove'></span></div></a>";
                     $("#dropboxDownload").append(list);
                 }
                 $(".file").on("mouseenter", function(event){
@@ -34,6 +34,25 @@ $(document).ready(function(){
                 $(".file").on("mouseleave",function(){
                     $("#tooltip").css("display","none");
                 });
+                $(".file").contextmenu(function() {
+                    let $temp = $("<input>");
+                    $("body").append($temp);
+                    $temp.val($(this).parent().attr("href")).select();
+                    document.execCommand("copy");
+                    $temp.remove();
+                });
+                $(".remove").on("click", function(){
+                    event.preventDefault();
+                    let id=$(this).parent().parent().attr("href").split("download/")[1];
+                    $.ajax({
+                        url: 'http://sodeok.xyz:8002/delete',
+                        type: 'POST',
+                        data:{id:id},
+                        success:function(data){
+                            if(data=="ok") location.reload();
+                        }
+                    });
+                })
             }
             else $("#dropboxDownload").html("<h2>공유 파일이 존재하지 않습니다!</h2>");
         }
