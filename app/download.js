e17d1bca10f400b1b88a965b3099c7f538e66503
snutiise <snutiise@hanmail.net>
2018-01-24 20:39:40 +0900

    Infinity

--- app/download.js
+++ app/download.js
@@ -3,7 +3,7 @@ function byteConvertor(bytes) {
     bytes = parseInt(bytes);
     var s = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
     var e = Math.floor(Math.log(bytes)/Math.log(1024));
-    if(e == "-Infinity") return "0 "+s[0]; 
+    if(e == -Infinity) return "0 "+s[0]; 
     else return (bytes/Math.pow(1024, Math.floor(e))).toFixed(2)+" "+s[e];
 }
 
